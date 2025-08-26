207f6c9aed4bfdc32d8d55c7fe0f73b84210983e
Fod9 <dernoncourt.tom@gmail.com>
2025-08-26 17:23:12 +0200

    print spec of a mlp network

--- main.c
+++ main.c
@@ -14,6 +14,12 @@ typedef struct {
     int num_layers;
 } NeuralNetwork;
 
+typedef struct {
+    int memory_used;
+    int num_neurons;
+    int num_weights;
+} LayerInfo;
+
 
 Layer init_layer(int input_size, int size, char act_function[30]) {
 
@@ -57,12 +63,14 @@ NeuralNetwork init_neural_network(int input_size, int output_size, int neuron_fo
     mlp.layers = malloc(number_of_layer * sizeof(Layer));
     int latest_input_size = input_size;
     
-    for(int neuron_index = 0; neuron_index < number_of_layer; neuron_index++) {
-        init_layer(latest_input_size, neuron_for_each_layers[neuron_index], "sigmoid");
+    for (int neuron_index = 0; neuron_index < number_of_layer; neuron_index++) {
+        mlp.layers[neuron_index] = init_layer(latest_input_size, neuron_for_each_layers[neuron_index], "sigmoid");
+        latest_input_size = neuron_for_each_layers[neuron_index];
     }
 
     return mlp;
 }
+
 
 void free_layer(Layer* layer) {
     if (layer->weights != NULL) {
@@ -78,20 +86,52 @@ void free_neural_network(NeuralNetwork* neural_network) {
     } 
 }
 
+LayerInfo calcul_layer_info(Layer layer) {
+    LayerInfo layer_info;
+    layer_info.num_neurons = layer.neuron_number;
+    layer_info.num_weights = layer_info.num_neurons*layer.input_size;
+    layer_info.memory_used = layer_info.num_weights*sizeof(float)+ layer_info.num_neurons * sizeof(float);
+
+    return layer_info;
+}
+
 int print_layer_specs(Layer layer) {
 
-    printf("Number of neurons : %d\n", layer.neuron_number);
-    printf("Number of weights : %d\n", layer.neuron_number * layer.input_size);
-    printf("Memory used for layer : %zu\n", layer.neuron_number * layer.input_size * sizeof(float));
+    LayerInfo layer_info = calcul_layer_info(layer);
+
+    printf("Number of neurons : %d\n", layer_info.num_neurons);
+    printf("Number of weights : %d\n", layer_info.num_weights);
+    printf("Memory used for layer : %d\n", layer_info.memory_used);
 
     return 1;
 };
 
+int print_mlp_specs(NeuralNetwork mlp) {
+    int number_of_weights = 0;
+    int nuumber_of_neurons = 0;
+    int memory_used = 0;
+    for(int layer_index = 0; layer_index < mlp.num_layers; layer_index++) {
+
+        LayerInfo layer_info = calcul_layer_info(mlp.layers[layer_index]);
+
+        number_of_weights += layer_info.num_weights;
+        nuumber_of_neurons += layer_info.num_neurons;
+        memory_used += layer_info.memory_used;
+
+    }
+    printf("Number of neurons : %d\n", nuumber_of_neurons);
+    printf("Number of weights %d\n", number_of_weights);
+    printf("Memory used %d\n", memory_used);
+
+    return 1;
+}
+
 
 int main() {
-    int neurons_for_each_layers[4] = {1,2,3,5};
+    int neurons_for_each_layers[7] = {16,32,64,128,256,512,256};
 
-    NeuralNetwork mlp = init_neural_network(1,2,neurons_for_each_layers, 4);
+    NeuralNetwork mlp = init_neural_network(200,5,neurons_for_each_layers, 6);
+    print_mlp_specs(mlp);
 
     free_neural_network(&mlp);
 
